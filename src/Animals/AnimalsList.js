d67d0f4df20f07716751d194d83b5955cd04bf59
BC-macair011 <mahialemu@gmail.com>
2021-11-02 14:02:29 +0200

    Changed the Animals list component from function to class component

--- src/Animals/AnimalsList.js
+++ src/Animals/AnimalsList.js
@@ -1,31 +1,61 @@
-import React from "react";
+// import React from "react";
+// import { animals } from "./animals";
+// import AnimalsCard from "./AnimalsCard";
+
+// import { Switch, Route, useRouteMatch } from "react-router-dom";
+// import AnimalSingle from "./AnimalSingle";
+
+// const AnimalsList = () => {
+//   const match = useRouteMatch();
+
+//   console.log(match);
+
+//   const animalslisting = animals.map((item) => (
+//     <AnimalsCard key={item.name} name={item.name} />
+//   ));
+
+//   return (
+//     <div className="animalsList">
+//       <Switch>
+//         <Route exact path={match.path}>
+//           {animalslisting}
+//         </Route>
+//         <Route path={`${match.path}/:animal`}>
+//           <AnimalSingle />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// };
+
+// export default AnimalsList;
+
+import React, { Component } from "react";
 import { animals } from "./animals";
 import AnimalsCard from "./AnimalsCard";
 
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import AnimalSingle from "./AnimalSingle";
 
-const AnimalsList = () => {
-  const match = useRouteMatch();
-
-  console.log(match);
-
-  const animalslisting = animals.map((item) => (
+class AnimalsList extends Component {
+  animalslisting = animals.map((item) => (
     <AnimalsCard key={item.name} name={item.name} />
   ));
 
-  return (
-    <div className="animalsList">
-      <Switch>
-        <Route exact path={match.path}>
-          {animalslisting}
-        </Route>
-        <Route path={`${match.path}/:animal`}>
-          <AnimalSingle />
-        </Route>
-      </Switch>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="animalsList">
+        <Switch>
+          <Route exact path={this.props.match.path}>
+            {this.animalslisting}
+          </Route>
+          <Route path={`${this.props.match.path}/:animal`}>
+            <AnimalSingle />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default AnimalsList;
