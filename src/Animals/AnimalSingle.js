22a7195607619cf3127f0ef3d96dfa4fd1abe86a
BC-macair011 <mahialemu@gmail.com>
2021-11-08 09:45:32 +0200

    on click of Read me link added a linking page that shous ´name of animal and picture of animal

--- src/Animals/AnimalSingle.js
+++ src/Animals/AnimalSingle.js
@@ -6,8 +6,25 @@ const AnimalSingle = () => {
   let history = useHistory();
 
   return (
-    <div>
-      <h1>This is the page for {animal}</h1>
+    <div className="singleInfo">
+      <div className="singleAnimal">
+        <h2 className="oneAnimalInfo" style={{ marginLeft: "10%" }}>
+          This is the page for {animal}
+        </h2>
+        <img
+          src={`https://source.unsplash.com/1600x900/?${animal}`}
+          alt="animal_picture"
+          style={{
+            width: "600px",
+            height: "400px",
+            marginTop: "2rem",
+            marginLeft: "2rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+      </div>
       <div>
         <button onClick={() => history.goBack()}>Back to animals</button>
       </div>
--- src/Animals/AnimalsList.js
+++ src/Animals/AnimalsList.js
@@ -30,30 +30,95 @@
 
 // export default AnimalsList;
 
+// import React, { Component } from "react";
+// import { animals } from "./animals";
+// import AnimalsCard from "./AnimalsCard";
+
+// import { Switch, Route } from "react-router-dom";
+// import AnimalSingle from "./AnimalSingle";
+
+// class AnimalsList extends Component {
+//   state = {
+//     searchInput: " ",
+//   };
+
+//   searchInputHandler = (event) => {
+//     this.setState({ searchInput: event.target.value });
+//     //console.log(this.state.searchInput);
+//   };
+//   animalsListing = animals.map((animal) => (
+//     <AnimalsCard key={animal.name} name={animal.name} />
+//   ));
+
+//   render() {
+//     const animalFilter = animals.filter((animal) => {
+//       return animal.name
+//         .toLowerCase()
+//         .includes(this.state.searchInput.toLowerCase());
+//     });
+//     const animalsListing = animalFilter.map((animal) => (
+//       <AnimalsCard key={animal.name} name={animal.name} />
+//     ));
+
+//     return (
+//       <div className="animalsList">
+//         <Switch>
+//           <Route exact path={this.props.match.path}>
+//             <h2>Search:</h2>
+//             <input type="text" onChange={this.searchInputHandler} />
+//             {this.animalsListing}
+//           </Route>
+//           <Route path={`${this.props.match.path}/:animal`}>
+//             <AnimalSingle />
+//           </Route>
+//         </Switch>
+//       </div>
+//     );
+//   }
+// }
+
+//export default AnimalsList;
 import React, { Component } from "react";
 import { animals } from "./animals";
 import AnimalsCard from "./AnimalsCard";
 
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import AnimalSingle from "./AnimalSingle";
 
 class AnimalsList extends Component {
-  animalslisting = animals.map((item) => (
-    <AnimalsCard key={item.name} name={item.name} />
+  state = {
+    searchInput: "",
+  };
+  searchInputHandler = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+  };
+  AnimalsList = animals.map((animal) => (
+    <AnimalsCard key={animal.name} name={animal.name} />
   ));
-
   render() {
+    const animalsFilter = animals.filter((animal) => {
+      return animal.name
+        .toLowerCase()
+        .includes(this.state.searchInput.toLowerCase());
+    });
+    const AnimalsList = animalsFilter.map((animal) => (
+      <AnimalsCard key={animal.name} name={animal.name} />
+    ));
     return (
-      <div className="animalsList">
-        <Switch>
-          <Route exact path={this.props.match.path}>
-            {this.animalslisting}
-          </Route>
-          <Route path={`${this.props.match.path}/:animal`}>
-            <AnimalSingle />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path={this.props.match.path}>
+          <div className="searchInput">
+            <h3 style={{ marginRight: ".5rem" }}>Search: </h3>
+            <input type="text" onChange={this.searchInputHandler}></input>
+          </div>
+          {AnimalsList}
+        </Route>
+        <Route path={`${this.props.match.path}/:animal`}>
+          <AnimalSingle />
+        </Route>
+      </Switch>
     );
   }
 }
